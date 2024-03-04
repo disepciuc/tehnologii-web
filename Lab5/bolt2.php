bc9c487644045d8427c3f6e6e8419a1beb201a3e
disepciuc <daniel.isepciuc@gmail.com>
2024-03-04 11:30:57 +0200

    Add files via upload

---
+++ Lab5/bolt2.php
@@ -0,0 +1,21 @@
+<?php
+
+// Completeaza codul lipsa
+
+// Conditii: 
+// 1. Este element numeric
+// 2. Numarul se divide cu 7
+// 3. Are in componenta 7 (Ex: 17)
+// Altfel afisam "Bolț"
+
+//Check Post variable, basic
+// if(isset($_POST['number'])) {
+//     echo htmlentities($_POST['number']); //cross-site scripting attac - XSS
+//     print_r($_POST);
+
+//     var_dump($_POST);
+// }
+
+//Check using filters
+
+?>
